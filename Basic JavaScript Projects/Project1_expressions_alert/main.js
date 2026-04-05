c05b538322ccf2b8f3271da240ecf1448d7dcad7
GAkyianu <goddie.akyianu@gmail.com>
2026-04-05 10:15:35 +0100

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -8,3 +8,5 @@ document.write(245 * 35);
 
 
 
+// Demontrating the use of the escape character in JavaScript strings
+document.write("\"I learned that courage was not the absence of fear, but the triumph over it.<br> The brave man is not he who does not feel afraid, but he who conquers that fear.\"- Nelson Mandela");
