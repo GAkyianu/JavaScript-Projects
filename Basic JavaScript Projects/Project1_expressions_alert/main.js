124591724a5c8409a3988018579413f1d1c48f49
GAkyianu <goddie.akyianu@gmail.com>
2026-04-05 07:23:57 +0100

    Assignment Submission
    JavaScript Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -1,5 +1,8 @@
 // Demonstrating a document.write() method
 document.write("Welcome to the world of JavaScript!");
+// Demonstrating a document.write statement
+document.write("Hello, World! This is my foremost Javascript staement!");
+
 
 
 
