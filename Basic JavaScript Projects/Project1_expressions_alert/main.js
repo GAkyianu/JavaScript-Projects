55677d8c322f31aada878777b9aa49b3bc101ea6
GAkyianu <goddie.akyianu@gmail.com>
2026-04-05 08:20:09 +0100

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -2,6 +2,9 @@
 document.write("Welcome to the world of JavaScript!");
 // Demonstrating a document.write statement
 document.write("Hello, World! This is my foremost Javascript staement!");
+// Creating two varibles
+var Sent_1 = "I travelled abroad last summer";
+var Sent_2 = " but I am doing a staycation this year";
 
 
 
