e6e9367ab52984f132cfc12dc807bdf0483b5707
GAkyianu <goddie.akyianu@gmail.com>
2026-04-11 14:08:51 +0100

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+    var String = "Hug me, I'm Ghanaian!";
+    var result = String.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = 
+    result;
+}
