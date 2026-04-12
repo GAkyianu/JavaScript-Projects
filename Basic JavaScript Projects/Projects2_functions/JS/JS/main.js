ece75730a079c48ba23866efa111cc48a987194c
GAkyianu <goddie.akyianu@gmail.com>
2026-04-12 06:42:11 +0100

    Project2_Function Assignment
    This is a submission of my Project2 Function Assignment

---
+++ Basic JavaScript Projects/Projects2_functions/JS/JS/main.js
@@ -0,0 +1,6 @@
+    // A function that uses the += operator
+    function myFunction() {
+    var sentence = "An apple a day keeps the doctor away"; // creating the variable
+    sentence += " is a popular adage"; // Using the += operator to add the variable
+    document.getElementById("Concatenate").innerHTML = sentence; // Using the document.getElementById method()
+    }
