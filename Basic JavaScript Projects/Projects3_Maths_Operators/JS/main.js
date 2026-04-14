cf1bc2a71f2aac87872029a12db18b2e180416bc
GAkyianu <goddie.akyianu@gmail.com>
2026-04-14 17:27:18 +0100

    Project3_Maths Operator Assignment
    A submission of my Project3 Maths Operator Assignment

--- Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
+++ Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
@@ -1,8 +1,4 @@
-function addition() { // function to perform
-    var add=7+8;
-    document.getElementById("Add").innerHTML="7 + 8 = " + add; // print the result in html
-
-function subtraction() { // function to perform
-    var sub=15-8;
-    document.getElementById("Sub").innerHTML="15 - 8 = " + sub; // print the result in html
+function multiplication() { // function to perform
+    var mult=15*8;
+    document.getElementById("Mult").innerHTML="15 * 8 = " + mult; // print the result in html
 }
