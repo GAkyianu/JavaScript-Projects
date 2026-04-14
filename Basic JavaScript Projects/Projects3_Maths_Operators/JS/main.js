41d55525bbf69a6094ce3dd8704cc7cef49fc7e4
GAkyianu <goddie.akyianu@gmail.com>
2026-04-14 09:01:46 +0100

    Update main.js

--- Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
+++ Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
@@ -1,4 +1,8 @@
 function addition() { // function to perform
     var add=7+8;
     document.getElementById("Add").innerHTML="7 + 8 = " + add; // print the result in html
+
+function subtraction() { // function to perform
+    var sub=15-8;
+    document.getElementById("Sub").innerHTML="15 - 8 = " + sub; // print the result in html
 }
