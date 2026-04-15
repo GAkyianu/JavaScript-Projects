ad8262baa63c3570cd0d5e6954aa7e504fea73d1
GAkyianu <goddie.akyianu@gmail.com>
2026-04-15 07:58:09 +0100

    A submission of my Project3 Maths Operator Assignment

--- Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
+++ Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
@@ -1,1 +1,4 @@
+function random() { // defining a function to perfom the random math operation
+    var ran=Math.random()*8;
+    document.getElementById("Ran").innerHTML="Maths.random()*8) = " + ran; // print the result in html
 }
