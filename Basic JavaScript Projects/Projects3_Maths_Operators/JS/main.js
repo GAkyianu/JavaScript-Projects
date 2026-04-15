a006334b5af5795cf45a45a044a1cfe3dcefa144
GAkyianu <goddie.akyianu@gmail.com>
2026-04-15 08:25:03 +0100

    A submission of my Project3 Maths Operator Assignment

--- Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
+++ Basic JavaScript Projects/Projects3_Maths_Operators/JS/main.js
@@ -1,5 +1,5 @@
-function increment() { // defining a function to perform the increment math operation
+function decrement() { // defining a function to perform the decrement math operation
     let x = 5; // declare a variable and assign it a value
-    x++; // increment the value of x by 1
-    document.getElementById("Inc").innerHTML="Incremented value = " + x; // print the result in html
+    x--; // decrement the value of x by 1
+    document.getElementById("Dec").innerHTML="Decremented value = " + x; // print the result in html
 }
