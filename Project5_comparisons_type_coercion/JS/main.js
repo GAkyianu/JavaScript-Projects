ef613b8631faf11515049457231b1225c768d55a
GAkyianu <goddie.akyianu@gmail.com>
2026-04-16 17:29:44 +0100

    Project5_Comparisons_Type_Coercion Assignment
    A submission of an various assignment on Project 5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,33 @@
+// main.js
+
+//using document.write and typeof to display the data type of af a variable
+document.write(typeof "looking forward to summer 2026"); 
+document.write("<br>");
+
+// An expression combining a string and a number
+document.write("15" + 10);
+document.write("<br>");
+
+// Utilizing the == operator
+document.write(9 == 9);
+document.write("<br>");
+
+// Utilizing the === operator
+a = 3;
+b = 3;
+document.write(a === b);
+document.write("<br>");
+
+// Utilizing the > and && operators
+document.write(13 > 7 && 12 > 8);
+document.write("<br>");
+
+// Utilizing the < and || operators
+document.write(9 < 6 || 8 < 7);
+document.write("<br>");
+
+// Utilizing the ! operator
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5 > 8);
+}
+
