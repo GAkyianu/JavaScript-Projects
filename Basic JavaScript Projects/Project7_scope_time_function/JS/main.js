23c09624094f09bd4bd313208530f584bf6e7d33
GAkyianu <goddie.akyianu@gmail.com>
2026-04-18 22:39:20 +0100

    Project7_Scope_Time_Function Assignment
    Submitting an assignment on an if and else function

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -3,6 +3,15 @@
 var A = 35; // Global variable
 function Add_numbers_1() {
     document.write(15 + A + "<br>"); 
+// A function with if and else statements
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 21) {
+        License = "You are eligible for a license!";
+    }
+    else {
+        License = "You are not eligible for a license!";
+    document.getElementById("How_old_are_you?").innerHTML = License 
 }
 function Add_numbers_2(){
     document.write(A + 45); // Using global variable
