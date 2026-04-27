cc65a55d1d001cc580adf1abead15af77d50dd21
GAkyianu <goddie.akyianu@gmail.com>
2026-04-27 17:15:25 +0100

    Project8_String_Methods
    Submitting an assignment on .toPrecision() method

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,5 +1,6 @@
-//Utilizing the .toString() method
-function string_Method() {
-    var myNumber = 312;
-    document.getElementById("ToString").innerHTML = myNumber.toString();
+// main.js
+//Utilizing the .toPrecision() method
+function precision_method() {
+    var X =  345.12345678910123;
+    document.getElementById("SpecificLength").innerHTML = X.toPrecision(7);
 }
