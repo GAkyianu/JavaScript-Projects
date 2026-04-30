0025a9b2aace6f249caf2d06ea4c784a041e0271
GAkyianu <goddie.akyianu@gmail.com>
2026-04-30 18:39:48 +0100

    Project10_Loops_Arrays
    A function using a for loop assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -7,6 +7,17 @@ function count_to_fifteen() {
     while (Y <= 15) {
         numbersString += "<br>" + Y;
         Y++;
+// A function using a for loop
+var Instrument = ["Clarinet", "Drums", "Piano", "Bass", "Voilin", "Trumpet", "Flute"];
+var Content =  "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instrument.length; Y++) {
+        Content += Instrument[Y] + "<br>";    
     }
     document.getElementById("Counting_to_fifteen").innerHTML = numbersString;
+    document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+
+
+
