21aaf882d1fe15765dfbf8a6911808aca7a77128
GAkyianu <goddie.akyianu@gmail.com>
2026-05-01 10:59:37 +0100

    Project10_Loops_Arrays
    A JavaScript Break Statement Assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,25 +1,14 @@
 // main.js
 
-// A function with an array
-function dog_acts() {
-    var Dog_Activity = [];
-    Dog_Activity[0] = "running";
-    Dog_Activity[1] = "sleeping";
-    Dog_Activity[2] = "playing";
-    Dog_Activity[3] = "barking";
-    document.getElementById("Dog").innerHTML = "In this activity, the dog is " + Dog_Activity[3] + ".";   
-// Creating an object with the let keyword
-function car(){
-    let car = {
-        make: "Audi",
-        year: 2017,
-        color: "Black",
-        model: "Q3"    
-    };
-    document.getElementById("My_Car_Object").innerHTML = 
-    "I own a " + car.year + " " + car.color + " " + car.make + " " + car.model + ".";
+// Creating a JavaScript Break Statement
+let text = "";
+
+for (let i = 0; i < 10; i++) {
+  if (i === 5) { break; }
+  text += "The number is " + i + "<br>";
 }
 
+document.getElementById("Trial").innerHTML = text;
 
 
 
