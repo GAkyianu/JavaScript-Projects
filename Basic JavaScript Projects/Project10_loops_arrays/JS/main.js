9db38435d7bf3f0ac9b3aedc1434d7bb38b4dd5a
GAkyianu <goddie.akyianu@gmail.com>
2026-05-01 10:36:23 +0100

    Project10_Loops_Arrays
    Creating an object using let assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -8,6 +8,16 @@ function dog_acts() {
     Dog_Activity[2] = "playing";
     Dog_Activity[3] = "barking";
     document.getElementById("Dog").innerHTML = "In this activity, the dog is " + Dog_Activity[3] + ".";   
+// Creating an object with the let keyword
+function car(){
+    let car = {
+        make: "Audi",
+        year: 2017,
+        color: "Black",
+        model: "Q3"    
+    };
+    document.getElementById("My_Car_Object").innerHTML = 
+    "I own a " + car.year + " " + car.color + " " + car.make + " " + car.model + ".";
 }
 
 
